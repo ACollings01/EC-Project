d3af31002af83c23c8f7112b8c95adfaa3f0ff6a
Alexander Collings <alexandercollings@protonmail.com>
2019-11-25 13:30:43 -0500

    Finished weapon rack model, began working on script for interactable objects

---
+++ Model Import Testing/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+	Light lightSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+		gameObject.AddComponent<Light>();
+		lightSource = gameObject.GetComponent<Light>();
+		lightSource.enabled = false;
+		lightSource.transform.position = new Vector3(lightSource.transform.position.x, lightSource.transform.position.y + 0.5f, lightSource.transform.position.z);
+		lightSource.intensity = 0.3f;
+		lightSource.color = Color.grey;
+    }
+
+	private void OnMouseEnter()
+	{
+		lightSource.enabled = true;
+		Debug.Log("Mouse has entered object space");
+	}
+
+	private void OnMouseExit()
+	{
+		lightSource.enabled = false;
+		Debug.Log("Mouse has left object space");
+	}
+}
